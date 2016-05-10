158fb644fc5ad77fa5d08463b70db4683bcceece
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2016-05-10 15:41:36 +0200

    Re-add crypto dependency

--- worker/project.clj
+++ worker/project.clj
@@ -21,7 +21,8 @@
                  [com.google.guava/guava "15.0"]
                  [me.raynes/fs "1.4.5"]
                  [org.rosuda/REngine "1.7.1-20130821.152906-1"]
-                 [com.novemberain/langohr "3.5.0"]]
+                 [com.novemberain/langohr "3.5.0"]
+                 [crypto-random "1.2.0"]]
   :env {:rserve-logs "log/rserve.log"}
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}}
