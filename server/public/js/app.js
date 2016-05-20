4daa54fa7a0f01f69a84274763f01e4480eba83b
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2016-05-20 12:33:04 +0200

    Support multiple tasks in dashboard

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -19,30 +19,40 @@ requirejs(['angular', 'patavi'], function(angular, patavi) {
     $scope.service = "slow";
     $scope.ttl = "PT5M";
     $scope.input = "{}";
+    $scope.tasks = [];
 
     $scope.submit = function(service, ttl, input) {
-      $scope.error = null;
-      $scope.status = null;
-      $scope.results = null;
+      var info = {
+        id: "<unknown>",
+        error: null,
+        status: null,
+        results: null,
+        warning: null
+      };
+      $scope.tasks.unshift(info);
 
       var taskUriPromise = $q.defer();
       var task = Patavi.listen(taskUriPromise.promise);
 
       $http.post('/task', input, { 'params': { 'service': service, 'ttl': ttl ? ttl : undefined } }).then(function(response) {
         if (response.status != 201) {
-          $scope.error = { 'message': 'Error queueing task: expected response status 201', 'status': response.status };
+          info.error = { 'message': 'Error queueing task: expected response status 201', 'status': response.status };
         } else if (response.data && response.data._links && response.data._links.updates && response.data._links.updates.href) {
+          if (response.data.status === "no-workers") {
+            info.warning = "No workers available";
+          }
+          info.id = response.data.id;
           taskUriPromise.resolve(response.data._links.updates.href);
         } else {
-          $scope.error = { 'message': 'Malformed response, expected _links.updates.href to be defined', 'data': response.data };
+          info.error = { 'message': 'Malformed response, expected _links.updates.href to be defined', 'data': response.data };
         }
       }, function(error) {
-        $scope.error = { 'message': 'Error queueing task', 'error': error };
+        info.error = { 'message': 'Error queueing task', 'error': error };
       });
 
       var handlerFactory = function(type) {
         return function(x) {
-          $scope[type] = x;
+          info[type] = x;
         };
       };
 
